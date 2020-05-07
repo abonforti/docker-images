64f9badb49ea8ee8411eea1eeb8b87a1c7a4e950
Alessio Bonforti <a.bonforti@accenture.com>
2020-05-07 10:32:21 +0200

    Modified scripts for bootstrapping

--- OracleDatabase/SingleInstance/dockerfiles/18.4.0/customScripts/01_createUser.sql
+++ OracleDatabase/SingleInstance/dockerfiles/18.4.0/customScripts/01_createUser.sql
@@ -1,5 +1,6 @@
 --ALTER SESSION SET CONTAINER=CUSTOMSCRIPTS;
-create tablespace hybrisSpace datafile '/u01/app/oracle/oradata/XE/hybrisSpace.dat' size 2048M;
+--create tablespace hybrisSpace datafile '/u01/app/oracle/oradata/XE/hybrisSpace.dat' size 2048M;
+create tablespace hybrisSpace datafile '/opt/oracle/oradata/XE/hybrisSpace.dat' size 2048M;
 create user hybris_usr identified by hybris_pwd default tablespace hybrisSpace quota 2047M on hybrisSpace;
 Grant all privileges to hybris_usr;
 
