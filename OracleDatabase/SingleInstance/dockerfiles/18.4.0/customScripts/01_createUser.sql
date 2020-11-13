aa8e759601485a73cdfee54832d3e176526aac6d
Alessio Bonforti <38355294+abonforti@users.noreply.github.com>
2020-11-13 09:38:02 +0100

    Update 01_createUser.sql

--- OracleDatabase/SingleInstance/dockerfiles/18.4.0/customScripts/01_createUser.sql
+++ OracleDatabase/SingleInstance/dockerfiles/18.4.0/customScripts/01_createUser.sql
@@ -1,5 +1,5 @@
 alter session set "_ORACLE_SCRIPT"=true;
-create or replace directory oracle_dump as '/tmp';
+create or replace directory oracle_dmp as '/tmp';
 create tablespace hybrisSpace datafile '/opt/oracle/oradata/XE/hybrisSpace.dat' size 2048M;
 create user hybris_usr identified by hybris_pwd default tablespace hybrisSpace quota 2047M on hybrisSpace;
 Grant all privileges to hybris_usr;
